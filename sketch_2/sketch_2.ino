47053010b257f096f29c1f6e0f6689433d0ba455
Edampadam Manu SIbi <manusibyedampadam@yahoo.it>
2019-02-06 13:13:15 +0100

    terza modifica

--- sketch_2/sketch_2.ino
+++ sketch_2/sketch_2.ino
@@ -1,6 +1,9 @@
 int Rosso = 12;
 int Giallo = 10;
 int Verde = 8;
+int Rosso2 = 6;
+int Giallo2 = 4;
+int Verde2 = 2;
 int tempo = 200;
 int ripetizioni = 4;
 void setup() {
@@ -13,10 +16,18 @@ void loop() {
   digitalWrite(Giallo, LOW);    
   digitalWrite(Verde, LOW);
   delay (3000);
+  digitalWrite(Rosso2, LOW);
+  digitalWrite(Giallo2, LOW);    
+  digitalWrite(Verde2, HIGH);
+  delay (3000);
   digitalWrite(Rosso, HIGH);
   digitalWrite(Giallo, HIGH);    
   digitalWrite(Verde, LOW);
   delay (3000); 
+  digitalWrite(Rosso2, LOW);
+  digitalWrite(Giallo2, LOW);    
+  digitalWrite(Verde2, HIGH);
+  delay (3000);
   digitalWrite(Rosso, LOW);
   digitalWrite(Giallo, LOW);    
   digitalWrite(Verde, HIGH);
